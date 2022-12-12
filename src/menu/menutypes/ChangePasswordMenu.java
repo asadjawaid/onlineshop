777a89b098b808e6e7ae6bd7b4cc34270d2d0752
Asad Jawaid <asadjawaid99@hotmail.com>
2022-12-12 10:22:45 -0500

    This menu allows a logged in user to update their password.

--- src/menu/menutypes/ChangePasswordMenu.java
+++ src/menu/menutypes/ChangePasswordMenu.java
@@ -1,15 +1,42 @@
 package menu.menutypes;
 
+import configs.ApplicationContext;
 import menu.Menu;
 
+import java.util.Scanner;
+
 public class ChangePasswordMenu implements Menu {
+    private final ApplicationContext context;
+
+    {
+        context = ApplicationContext.getInstance();
+    }
+
+    /**
+     * This function
+     */
     @Override
     public void start() {
-
+        printMenuHeader();
+        changePassword();
     }
 
     @Override
     public void printMenuHeader() {
+        System.out.println("***** CHANGE PASSWORD ****");
+    }
 
+    /**
+     * Changes a logged in user's password
+     */
+    private void changePassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter new password: ");
+        String newPassword = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("Your password has been successfully changed.");
+        context.getLoggedInUser().setPassword(newPassword); // set the new password
     }
 }
