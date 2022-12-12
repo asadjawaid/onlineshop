d4bc408bde49a8ca0b4672cf30e507888600d8f9
Asad Jawaid <asadjawaid99@hotmail.com>
2022-12-12 10:23:15 -0500

    This menu allows a user to update their email.

--- src/menu/menutypes/ChangeEmailMenu.java
+++ src/menu/menutypes/ChangeEmailMenu.java
@@ -1,15 +1,44 @@
 package menu.menutypes;
 
+import configs.ApplicationContext;
 import menu.Menu;
+import services.UserManagementService;
+import services.impl.DefaultUserManagementService;
+
+import java.util.Scanner;
 
 public class ChangeEmailMenu implements Menu {
+    private final ApplicationContext context;
+
+    {
+        context = ApplicationContext.getInstance();
+    }
+
     @Override
     public void start() {
+        if (context.getLoggedInUser() == null) {
+            System.out.println("Please, log in or create a new account to change your account settings.");
+            context.getMainMenu().start();
+            return;
+        }
 
+        printMenuHeader();
+        changeEmail();
+    }
+
+    public void changeEmail() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter new email: ");
+        String newEmail = scanner.nextLine();
+
+        scanner.close();
+
+        context.getLoggedInUser().setEmail(newEmail); // updates the email for the current logged in user
+        System.out.println("Your email has been successfully changed!");
     }
 
     @Override
     public void printMenuHeader() {
-
+        System.out.println("***** CHANGE EMAIL *****");
     }
 }
